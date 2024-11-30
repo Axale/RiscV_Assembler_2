c5b68a2e8c1bd44fe39ac5759baa454108063973
Will Hagan <38008781+Axale@users.noreply.github.com>
2024-11-30 16:21:19 -0500

    starting

--- include/interface.hpp
+++ include/interface.hpp
@@ -1,20 +1,44 @@
- 
-// 
-class parsed_node {
+#include <vector>
+#include <string>
+#include <list>
+#include <iterator>
+
+using namespace std;
+
+// Node for the list of opcodes.
+// Has the address so I can easily do org instructions. 
+// Its mainly a data storage class, so no fancy methods
+class ParsedNode {
 private:
   	int address;
-  	int opcode;
+  	int instruction;
 public:
-  	parsed_node(int address, int opcode);
-  	~parsed_node();
-	int get_address();
-	int get_opcode();
+  	ParsedNode(int address, int instruction);
+  	~ParsedNode();
+	
+	// Getters/Setters
+	int get_address(); void set_address(int address);
+	int get_instruction(); void set_instruction(int instruction);
 };
 
-class interface {
+// Data interface between File I/O and Parser
+// 
+class Interface {
 private:
-  	/* data */
+	list<string*> lines;
+	list<string*>::iterator lines_it;
+
+	list<ParsedNode*> parsed_list;
+	list<ParsedNode*>::iterator parsed_it;
 public:
-  	interface();
-  	~interface();
+  	Interface();
+  	~Interface();
+
+	// Adding functions
+	int add_line(string* line);
+	int add_parsed(ParsedNode Node);
+
+	// Step functions 
+	string* step_lines();
+	ParsedNode* step_parsed();
 };
---
+++ src/interface.cpp
@@ -0,0 +1,5 @@
+#include "interface.hpp"
+
+Interface::Interface() {
+  self. 
+}
