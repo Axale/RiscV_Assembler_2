3dd8fceb1f63850895d9eae84ab0abdc6654486e
Axale <38008781+Axale@users.noreply.github.com>
2024-12-08 16:01:41 -0500

    Started on a unit test setup.

--- apps/io-test/config/config.cmake
+++ apps/io-test/config/config.cmake
@@ -1,3 +1,1 @@
-
-
 add_compile_definitions(TEST_IO)
--- apps/io-test/src/main.cpp
+++ apps/io-test/src/main.cpp
@@ -1,2 +1,17 @@
 #include "interface.hpp"
 #include "fileio.hpp"
+
+// This function has two objects: one is the actual object that is being tested, and the other has the expected state of the object.
+template <typename T> class Tester {
+    private:
+    T* actual;
+    T* expected;
+    
+    
+};
+
+int main(void) {
+    Interface DI();
+
+
+}
