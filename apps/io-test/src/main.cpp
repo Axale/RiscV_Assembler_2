709b6547d84dade29b172516c9fc7cfb4c3573d1
Axale <38008781+Axale@users.noreply.github.com>
2024-12-16 21:54:59 -0500

    Fixed up testing more, started debugging prep_line

--- apps/io-test/src/main.cpp
+++ apps/io-test/src/main.cpp
@@ -1,92 +1,105 @@
 #define IO_TEST
 #include "interface.hpp"
 #include "fileio.hpp"
-#include "nlohmann/json.hpp"
+#include <nlohmann/json.hpp>
 #include <vector>
 #include <string>
 #include <fstream>
 #include <iostream>
 
-// This function has two objects: one is the actual object that is being tested, and the other has the expected state of the object.
-template <typename T, typename U, typename F> class Tester {
+template <typename IN, typename OUT, typename RET> class Tester {
     public:
+
     class Test {
         public:
         int test_num;
         std::string test_name;
         bool expected_success;
-        T expected_output;
-        U input;
-        Test() : test_num(0), test_name(""), expected_success(false), expected_output(""), input() {}
+        RET expected_return;
+        OUT expected_output;
+        IN input;
+        Test() : test_num(0), test_name(""), expected_success(false), expected_return(),expected_output(), input() {}
     };
     
     private:
-    T actual;
-    FileIO FIO;
 
     std::vector<Test> test_vec;
-
-    F function_under_test;
-    bool chk_out(Test& t) {
-        if(t.expected_output == actual) {
-            return t.expected_success;
-        } else {
-            return !(t.expected_success);
-        }
-    }
-
-
+    // check function
+    bool (*t_func)(IN&, OUT&, RET&);
     public:
 
-    Tester(std::string& json_path, F funder_test) {
-        function_under_test = funder_test;
+    Tester(bool (*t_fun)(IN&, OUT&, RET&)) {
+        t_func = t_fun;
     }
 
+
+    // Initializes an array of tests from a json file.
+    // Returns an into to makr an error
     int init_tests(std::string& path) {
+        
         ifstream js_file(path.c_str(), std::ifstream::in);
         if(js_file.fail()) {
             return -1;
         }
 
         nlohmann::json jdata = nlohmann::json::parse(js_file);
+        
+        if(jdata.empty()) {
+            return -1;
+        }
+
         test_vec = jdata.get<std::vector<Test>>();
         js_file.close();
         return 0;
     }
     
     void run_tests() {
+
+        // loops through the test vector
         for(Test obj : test_vec) {
-            actual = (FIO.*function_under_test)(obj.input);
-            if(chk_out(obj) == false) {
-                std::cout << "Failed at test #" << obj.test_num << ": " << obj.test_name << ".\n";
+                std::cout << "\nTest #" << obj.test_num << ": " << obj.test_name << ".\n";
+            bool success = t_func(obj.input, obj.expected_output, obj.expected_return);
+            if(success == false) {
+                std::cout << "Test Failed!\n";
             } else {
-                std::cout << "Test #" << obj.test_num << " passed.\n";
+                std::cout << "Test Passed!\n";
             }
         }
     }
 };
+
 
 void from_json(const nlohmann::json& js, ParsedNode& pn){
 	js.at("address").get_to(pn.address);
 	js.at("instruction").get_to(pn.instruction);
 }
 
-void from_json(const nlohmann::json& j, Tester<std::string, ParsedNode,  std::string (FileIO::*)(ParsedNode&)>::Test& t) {
+void from_json(const nlohmann::json& j, Tester<ParsedNode, std::string, std::string>::Test& t) {
     j.at("test_num").get_to(t.test_num);
     j.at("test_name").get_to(t.test_name);
     j.at("expected_success").get_to(t.expected_success);
     j.at("expected_output").get_to(t.expected_output);
+    j.at("expected_return").get_to(t.expected_return);
     j.at("input").get_to(t.input);
+}
+
+FileIO fio;
+bool prep_line_chk(ParsedNode& Input, std::string& NA, std::string& expected_return) {
+    std::string act_return = fio.prep_line_wrapper(Input);
+    std::cout << "Expected: " << expected_return << "\n";
+    std::cout << "Actual:   " << act_return << "\n";
+    return act_return == expected_return;
 }
 
 int main(void) {
     std::string json_path("C:\\Users\\wbhag\\Desktop\\Python Stuff\\riscv_assembler_v2\\json\\test1.json") ;
-    Tester< std::string,  ParsedNode,  std::string (FileIO::*)(ParsedNode&)> tester(json_path, &(FileIO::prep_line_wrapper));
+    Tester<ParsedNode, std::string, std::string> tester(&(prep_line_chk));
     if(tester.init_tests(json_path) < 0) {
         return -1;
     }
 
     tester.run_tests();
+    std::cout << "All Tests Finished!\n";
     return 0;
 
 }
--- modules/fileio/fileio.cpp
+++ modules/fileio/fileio.cpp
@@ -28,24 +28,24 @@ int FileIO::file_input(string& file_name, Interface& data_interface) {
 
 string FileIO::prep_line(ParsedNode& Node) {
     stringstream stream ;
-    if (Node.get_address() >= 0x8000 << 2) {
+    if (Node.get_address() >= 0xFFFF ) {
         return "";
     }
     stream << ":04"
     << std::setfill('0') << std::setw(sizeof(int32_t) )
-    << std::hex << Node.get_address();
+    << std::uppercase << std::hex << Node.get_address();
     
     stream << "00" << std::setfill('0') << std::setw(sizeof(int16_t))
-    << std::hex << Node.get_instruction();
+    << std::uppercase << std::hex << Node.get_instruction();
     
     auto inter = stream.str();
     int sum = 0;
     for(auto inter_it = inter.begin() + 1; inter_it != inter.end(); inter_it++) {
         sum += (inter_it - inter.begin()) % 2 ? *inter_it * 16 : *inter_it;
     }
-    sum *= 0xFF;
+    sum &= 0xFF;
     sum = ~sum + 1;
-    stream << setfill('0') << setw(sizeof(char)) << std::hex << sum << "\n";
+    stream << setfill('0') << setw(sizeof(short int)) << std::uppercase << std::hex <<(short int) sum << "\n";
     return stream.str();
 }
 
